de482e128e7d851d5c47a8b7db1229a121e8e444
Iamneo <techadmin@iamneo.ai>
2023-09-22 04:44:08 +0000

    Backup Commit

--- test.tf
+++ test.tf
@@ -2,11 +2,11 @@ terraform{
      required_providers {
        aws = {
            source = "hashicorp/aws"
-           version="~>5.0"
-     }
+        }
      }  
 }
 provider "aws"{
     region = "Singapore"
-    acce
+    access_key=""
+    secret_key=""
 }
